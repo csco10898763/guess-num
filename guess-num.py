d477b103309f5bd39e49b37e4aeb73be39050454
Ken Kuo <csco10898763@gmail.com>
2021-11-10 15:00:50 +0800

    third time

--- guess-num.py
+++ guess-num.py
@@ -4,7 +4,7 @@
 # 猜錯的話 要告訴他 比答案 大 / 小
 
 import random
-i = 0 
+count = 0 
 x = 0
 y = 0
 x = int(input('請輸入起始數字：'))
@@ -12,11 +12,11 @@ y = int(input('請輸入結束數字：'))
 r = random.randint(x, y)
 
 while True:
-    i = i + 1
-    num = input('請猜數字：')
-    num = int(num)
+    count += 1 # count = count + 1
+    num = int(input('\n請猜數字：'))
+    print('\n這是你的猜第', count, '次')
+    
     if num == r: 
-        print('您猜了有', i, '次')
         print('\n終於猜對了 !!!')
         break
     elif num > r:
